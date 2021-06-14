7b61e023accf38f58b893159e04f33e3335072a9
siddhant-28 <47878062+siddhant-28@users.noreply.github.com>
2021-06-14 14:12:29 -0700

    Add files via upload
    Website hosting

---
+++ helper.js
@@ -0,0 +1,26 @@
+var cols = 30;
+var rows = 30;
+var grid = new Array(cols);
+var openSet = [];
+var closedSet = [];
+var start;
+var end;
+var w , h;
+var path = [];
+var noSolution = false;
+var startClicked = false;
+var cnv;
+var current;
+
+function removefromArray(arr, elt) {
+    for( var i = arr.length - 1; i >= 0; i--) {
+        if(arr[i] == elt) {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+function heuristic(a, b) {
+    var d = abs(a.i - b.i) + abs(a.j - b.j);
+    return d;
+}
